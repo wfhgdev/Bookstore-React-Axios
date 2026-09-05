0cbcb7ee5e57ce986624a565b38bb4dd586a3a42
wfhgdev <willifhernandez@gmail.com>
2026-09-05 21:18:45 +0200

    feat: initialize project structure with React Router and Tailwind CSS integration

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,9 @@
 const Home = () => {
   return (
     <h1 className="text-3xl font-bold text-center mt-10">
-        Home de My Library 🏠
-      </h1>
+      Home de My Library 🏠
+    </h1>
   )
 }
 
-export default Home
+export default Home;
